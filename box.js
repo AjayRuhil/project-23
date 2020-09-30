b0c8163c3ff3519e29d587363357db8fec07eece
AjayRuhil <71335040+AjayRuhil@users.noreply.github.com>
2020-09-30 09:54:28 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,9 @@
+class box{
+
+ this.body = Bodies.rectangle(200, 100, 10,70);
+World.add(world. this.body);
+
+display()
+var pos =this.body.position;
+rect(pos.x, pos.y, this.width, this.height);
+}
